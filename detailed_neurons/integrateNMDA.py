3f49b4ed6096fed84cabd437e6ede0b629a8ad1e
Peter Duggins <psipeter@gmail.com>
2020-11-16 22:43:06 -0500

    moderate loss in accuracy with fixed filters and NMDA voltage-gating

--- detailed_neurons/integrateNMDA.py
+++ detailed_neurons/integrateNMDA.py
@@ -42,14 +42,14 @@ def makeSin(t, f, dt=0.001, seed=0):
     sin = lambda t: mag*np.sin(freq*t + phase)
     return sin
 
-def go(NPre=100, N=100, t=10, m=Uniform(30, 30), i=Uniform(-0.8, 0.8), seed=0, dt=0.001, fPre=None, fBio=None, fS=DoubleExp(1e-3, 1e-1), dPreA=None, dPreB=None, dBio=None, ePreA=None, ePreB=None, eBio=None, stage=None, alpha=1e-7, eMax=1e-1, stimA=lambda t: np.sin(t), stimB=lambda t: 0):
+def go(NPre=100, N=100, t=10, m=Uniform(30, 30), i=Uniform(-0.8, 0.8), seed=0, dt=0.001, fPre=None, fNMDA=None, fS=DoubleExp(1e-3, 1e-1), dPreA=None, dPreB=None, dBio=None, ePreA=None, ePreB=None, eBio=None, stage=None, alpha=1e-7, eMax=1e-1, stimA=lambda t: np.sin(t), stimB=lambda t: 0, DA=lambda t: 1.0):
 
     with nengo.Network(seed=seed) as model:
         inptA = nengo.Node(stimA)
         inptB = nengo.Node(stimB)
         preInptA = nengo.Ensemble(NPre, 1, radius=3, max_rates=m, seed=seed)
         preInptB = nengo.Ensemble(NPre, 1, max_rates=m, seed=seed)
-        ens = nengo.Ensemble(N, 1, max_rates=m, intercepts=i, neuron_type=Bio("Pyramidal"), seed=seed)
+        ens = nengo.Ensemble(N, 1, max_rates=m, intercepts=i, neuron_type=Bio("Pyramidal", DA=DA), seed=seed)
         tarEns = nengo.Ensemble(N, 1, max_rates=m, intercepts=i, neuron_type=nengo.LIF(), seed=seed)
         cpa = nengo.Connection(inptA, preInptA, synapse=None, seed=seed)
         cpb = nengo.Connection(inptB, preInptB, synapse=None, seed=seed)
@@ -62,31 +62,31 @@ def go(NPre=100, N=100, t=10, m=Uniform(30, 30), i=Uniform(-0.8, 0.8), seed=0, d
         if stage==1:
             c0b = nengo.Connection(preInptB, tarEns, synapse=fPre, solver=NoSolver(dPreB), seed=seed+1)
             c1b = nengo.Connection(preInptB, ens, synapse=fPre, solver=NoSolver(dPreB), seed=seed+1)
-            learnEncoders(c1b, tarEns, fS, alpha=alpha, eMax=eMax)
+            learnEncoders(c1b, tarEns, fS, alpha=alpha, eMax=eMax, tTrans=1.0)
         if stage==2:
             c0a = nengo.Connection(preInptA, tarEns, synapse=fPre, solver=NoSolver(dPreA), seed=seed)
             c0b = nengo.Connection(preInptB, tarEns, synapse=fPre, solver=NoSolver(dPreB), seed=seed+1)
             c1a = nengo.Connection(preInptA, ens, synapse=fPre, solver=NoSolver(dPreA), seed=seed)
             c1b = nengo.Connection(preInptB, ens, synapse=fPre, solver=NoSolver(dPreB), seed=seed+1)
-            learnEncoders(c1a, tarEns, fS, alpha=alpha, eMax=eMax)
+            learnEncoders(c1a, tarEns, fS, alpha=alpha, eMax=eMax, tTrans=1.0)
         if stage==3:
             c1a = nengo.Connection(preInptA, ens, synapse=fPre, solver=NoSolver(dPreA), seed=seed)
             c1b = nengo.Connection(preInptB, ens, synapse=fPre, solver=NoSolver(dPreB), seed=seed+1)
         if stage==4:
             preInptC = nengo.Ensemble(NPre, 1, max_rates=m, seed=seed)
-            ens2 = nengo.Ensemble(N, 1, max_rates=m, intercepts=i, neuron_type=Bio("Pyramidal"), seed=seed)
-            ens3 = nengo.Ensemble(N, 1, max_rates=m, intercepts=i, neuron_type=Bio("Pyramidal"), seed=seed)
-            nengo.Connection(inptB, preInptC, synapse=fBio, seed=seed)
+            ens2 = nengo.Ensemble(N, 1, max_rates=m, intercepts=i, neuron_type=Bio("Pyramidal", DA=DA), seed=seed)
+            ens3 = nengo.Ensemble(N, 1, max_rates=m, intercepts=i, neuron_type=Bio("Pyramidal", DA=DA), seed=seed)
+            nengo.Connection(inptB, preInptC, synapse=fNMDA, seed=seed)
             c1a = nengo.Connection(preInptA, ens, synapse=fPre, solver=NoSolver(dPreA), seed=seed)
             c2b = nengo.Connection(preInptB, ens2, synapse=fPre, solver=NoSolver(dPreB), seed=seed+1)
-            c3 = nengo.Connection(ens2, ens, synapse=fBio, solver=NoSolver(dBio), seed=seed)
+            c3 = nengo.Connection(ens2, ens, synapse=NMDA(), solver=NoSolver(dBio), seed=seed)
             c4a = nengo.Connection(preInptA, ens3, synapse=fPre, solver=NoSolver(dPreA), seed=seed)
             c4b = nengo.Connection(preInptC, ens3, synapse=fPre, solver=NoSolver(dPreB), seed=seed)
-            learnEncoders(c3, ens3, fS, alpha=alpha, eMax=eMax)
+            learnEncoders(c3, ens3, fS, alpha=alpha, eMax=eMax, tTrans=1.0)
             pTarEns = nengo.Probe(ens3.neurons, synapse=None)
         if stage==5:
             c1a = nengo.Connection(preInptA, ens, synapse=fPre, solver=NoSolver(dPreA), seed=seed)
-            c5 = nengo.Connection(ens, ens, synapse=fBio, solver=NoSolver(dBio), seed=seed)
+            c5 = nengo.Connection(ens, ens, synapse=NMDA(), solver=NoSolver(dBio), seed=seed)
 
     with nengo.Simulator(model, seed=seed, dt=dt, progress_bar=False) as sim:
         if stage==1:
@@ -128,7 +128,7 @@ def go(NPre=100, N=100, t=10, m=Uniform(30, 30), i=Uniform(-0.8, 0.8), seed=0, d
     )
 
 
-def run(NPre=100, N=100, t=10, nTrain=10, nTest=3, nEnc=10, dt=0.001, fPre=DoubleExp(1e-3, 1e-1), fS=DoubleExp(1e-3, 1e-1), Tff=0.3, reg=1e-1, tauRiseMax=3e-3, tauFallMax=3e-1, load=[], file=None):
+def run(NPre=100, N=100, t=10, nTrain=10, nTest=3, nEnc=10, dt=0.001, fPre=DoubleExp(1e-3, 1e-1), fNMDA=DoubleExp(10.6e-3, 285e-3), fS=DoubleExp(1e-3, 1e-1), Tff=0.3, reg=1e-1, tauRiseMax=3e-3, tauFallMax=3e-1, load=[], file=None):
 
     if 1 in load:
         dPreA = np.load(file)['dPreA'] # fix indexing of decoders
@@ -179,9 +179,9 @@ def run(NPre=100, N=100, t=10, nTrain=10, nTest=3, nEnc=10, dt=0.001, fPre=Doubl
 
     if 4 in load:
         dBio = np.load(file)['dBio']
-        tauRiseBio = np.load(file)['tauRiseBio']
-        tauFallBio = np.load(file)['tauFallBio']
-        fBio = DoubleExp(tauRiseBio, tauFallBio)
+#         tauRiseBio = np.load(file)['tauRiseBio']
+#         tauFallBio = np.load(file)['tauFallBio']
+#         fNMDA = DoubleExp(tauRiseBio, tauFallBio)
     else:
         print('readout decoders for ens')
         spikes = np.zeros((nTrain, int(t/dt), N))
@@ -191,9 +191,10 @@ def run(NPre=100, N=100, t=10, nTrain=10, nTest=3, nEnc=10, dt=0.001, fPre=Doubl
             data = go(dPreA=dPreA, dPreB=dPreB, ePreA=ePreA, ePreB=ePreB, NPre=NPre, N=N, t=t, dt=dt, fPre=fPre, fS=fS, stimA=stimA, stimB=stimB, stage=3)
             spikes[n] = data['ens']
             targets[n] = fPre.filt(data['inptB'], dt=dt)
-        dBio, fBio, tauRiseBio, tauFallBio, X2, Y2, error2 = decode(spikes, targets, nTrain, dt=dt, reg=reg, tauRiseMax=tauRiseMax, tauFallMax=tauFallMax, name="integrateNMDA")
-#         dBio, X2, Y2, error2 = decodeNoF(spikes, targets, nTrain, f, dt=0.001, reg=reg)
-        np.savez("data/integrateNMDA.npz", dPreA=dPreA, dPreB=dPreB, ePreA=ePreA, ePreB=ePreB, dBio=dBio, tauRiseBio=tauRiseBio, tauFallBio=tauFallBio)
+#         dBio, fNMDA, tauRiseBio, tauFallBio, X2, Y2, error2 = decode(spikes, targets, nTrain, dt=dt, reg=reg, tauRiseMax=tauRiseMax, tauFallMax=tauFallMax, name="integrateNMDA")
+        dBio, X2, Y2, error2 = decodeNoF(spikes, targets, nTrain, fNMDA, dt=0.001, reg=reg)
+#         np.savez("data/integrateNMDA.npz", dPreA=dPreA, dPreB=dPreB, ePreA=ePreA, ePreB=ePreB, dBio=dBio, tauRiseBio=tauRiseBio, tauFallBio=tauFallBio)
+        np.savez("data/integrateNMDA.npz", dPreA=dPreA, dPreB=dPreB, ePreA=ePreA, ePreB=ePreB, dBio=dBio)
         times = np.arange(0, t*nTrain, dt)
         plotState(times, X2, Y2, error2, "integrateNMDA", "ens", t*nTrain)
 
@@ -204,17 +205,18 @@ def run(NPre=100, N=100, t=10, nTrain=10, nTest=3, nEnc=10, dt=0.001, fPre=Doubl
         eBio = np.zeros((N, N, 1))
         for n in range(nEnc):
             stimA, stimB = makeSignal(t, fPre, dt=0.001, seed=n)
-            data = go(dPreA=dPreA, dPreB=dPreB, dBio=dBio, ePreA=ePreA, ePreB=ePreB, eBio=eBio, NPre=NPre, N=N, t=t, dt=dt, fPre=fPre, fBio=fBio, fS=fS, stimA=stimA, stimB=stimB, stage=4, alpha=1e-7)
+            data = go(dPreA=dPreA, dPreB=dPreB, dBio=dBio, ePreA=ePreA, ePreB=ePreB, eBio=eBio, NPre=NPre, N=N, t=t, dt=dt, fPre=fPre, fNMDA=fNMDA, fS=fS, stimA=stimA, stimB=stimB, stage=4, alpha=1e-7)
             eBio = data['eBio']
             plotActivity(t, dt, fS, data['times'], data['ens'], data['tarEns'], "integrateNMDA", "Ens2Ens")
-            np.savez("data/integrateNMDA.npz", dPreA=dPreA, dPreB=dPreB, ePreA=ePreA, ePreB=ePreB, dBio=dBio, tauRiseBio=tauRiseBio, tauFallBio=tauFallBio, eBio=eBio)
+#             np.savez("data/integrateNMDA.npz", dPreA=dPreA, dPreB=dPreB, ePreA=ePreA, ePreB=ePreB, dBio=dBio, tauRiseBio=tauRiseBio, tauFallBio=tauFallBio, eBio=eBio)
+            np.savez("data/integrateNMDA.npz", dPreA=dPreA, dPreB=dPreB, ePreA=ePreA, ePreB=ePreB, dBio=dBio, eBio=eBio)
 
     print("testing")
     errors = np.zeros((nTest))
     for test in range(nTest):
         stimA, stimB = makeSignal(t, fPre, dt=0.001, seed=200+test)
-        data = go(dPreA=dPreA, dBio=dBio, ePreA=ePreA, eBio=eBio, NPre=NPre, N=N, t=t, dt=dt, fPre=fPre, fBio=fBio, fS=fS, stimA=stimA, stimB=stimB, stage=5)
-        A = fBio.filt(data['ens'], dt=dt)
+        data = go(dPreA=dPreA, dBio=dBio, ePreA=ePreA, eBio=eBio, NPre=NPre, N=N, t=t, dt=dt, fPre=fPre, fNMDA=fNMDA, fS=fS, stimA=stimA, stimB=stimB, stage=5)
+        A = fNMDA.filt(data['ens'], dt=dt)
         X = np.dot(A, dBio)
         Y = fPre.filt(data['inptB'], dt=dt)
         U = fPre.filt(Tff*data['inptA'], dt=dt)
@@ -231,7 +233,8 @@ def run(NPre=100, N=100, t=10, nTrain=10, nTest=3, nEnc=10, dt=0.001, fPre=Doubl
         fig.savefig("plots/integrateNMDA_test%s.pdf"%test)
         plt.close('all')
     print('errors:', errors)
-    np.savez("data/integrateNMDA.npz", dPreA=dPreA, dPreB=dPreB, ePreA=ePreA, ePreB=ePreB, dBio=dBio, tauRiseBio=tauRiseBio, tauFallBio=tauFallBio, eBio=eBio, errors=errors)
+#     np.savez("data/integrateNMDA.npz", dPreA=dPreA, dPreB=dPreB, ePreA=ePreA, ePreB=ePreB, dBio=dBio, tauRiseBio=tauRiseBio, tauFallBio=tauFallBio, eBio=eBio, errors=errors)
+    np.savez("data/integrateNMDA.npz", dPreA=dPreA, dPreB=dPreB, ePreA=ePreA, ePreB=ePreB, dBio=dBio, eBio=eBio, errors=errors)
     return errors
 
-errors = run(N=30, nTrain=10, nEnc=10, load=[1,2,3], nTest=10, file="data/integrateNMDA.npz")
+errors = run(N=30, nTrain=10, nEnc=10, load=[], nTest=10, file="data/integrateNMDA.npz")

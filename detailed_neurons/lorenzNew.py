491581eadfd4c3e48e72499e2774a5400eb23b73
Peter Duggins <psipeter@gmail.com>
2020-12-12 21:19:56 -0500

    only a few inf errors for lorenz wilson

--- detailed_neurons/lorenzNew.py
+++ detailed_neurons/lorenzNew.py
@@ -1,14 +1,12 @@
 import numpy as np
 import nengo
 from scipy.ndimage import gaussian_filter1d
-# from scipy.optimize import curve_fit
-# from scipy.signal import find_peaks
 from nengo.dists import Uniform
 from nengo.solvers import NoSolver
 from nengo.utils.numpy import rmse
 from nengolib import Lowpass, DoubleExp
 from nengolib.signal import s
-from utils import dfOpt, WNode, learnEncoders, setWeights, decode, plotState, plotActivity, DownsampleNode, plot3D, plotPairwise, plotTent
+from utils import dfOpt, WNode, learnEncoders, setWeights, decode, plotState, plotActivity, DownsampleNode, plot3D, plotPairwise, plotTent, plotLorenz
 from neurons import LIF, ALIF, Wilson, Bio, reset_neuron
 import neuron
 import matplotlib.pyplot as plt
@@ -28,11 +26,10 @@ def feedback(x):
 def mountain(x, a, b, c):
     return (a*x + b)*(x<=c) + (-a*x + 2*a*c + b)*(x>c)
 
-def go(N=3000, d1=None, f1=None, t=100, l=False, neuron_type=LIF(),
-       m=Uniform(30, 30), i=Uniform(-0.8, 0.8), r=30, IC=np.array([1,1,1]),
-       seed=0, dt=0.001, dtSample=0.001, f=DoubleExp(1e-3, 1e-1)):
+def go(N=2000, d=None, f=None, t=100, l=False, neuron_type=LIF(),
+       m=Uniform(30, 30), i=Uniform(-1, 1), r=30, IC=np.array([0,0,0]),
+       seed=0, dt=0.001, dtSample=0.001):
 
-    if not f1: f1 = f
     with nengo.Network(seed=seed) as model:
         inpt = nengo.Node(lambda t: IC*(t<=1.0))
         tar = nengo.Ensemble(1, 3, neuron_type=nengo.Direct())
@@ -41,73 +38,83 @@ def go(N=3000, d1=None, f1=None, t=100, l=False, neuron_type=LIF(),
         nengo.Connection(inpt, tar, synapse=None)
         nengo.Connection(tar, tar, function=feedback, synapse=~s)
         if l:
-            nengo.Connection(tar, ens, synapse=f, seed=seed)
+            nengo.Connection(tar, ens, synapse=None, seed=seed)
         else:
             nengo.Connection(inpt, ens, synapse=None, seed=seed)
-        nengo.Connection(ens, ens, synapse=f1, solver=NoSolver(d1), seed=seed)
+            nengo.Connection(ens, ens, synapse=f, solver=NoSolver(d), seed=seed)
         nengo.Connection(ens.neurons, dss, synapse=None)
         pTar = nengo.Probe(tar, synapse=None, sample_every=dtSample)
         pEns = nengo.Probe(dss, synapse=None, sample_every=dtSample)
 
     with nengo.Simulator(model, seed=seed, dt=dt, progress_bar=False) as sim:
-        sim.run(t, progress_bar=True)
+        sim.run(t+dt, progress_bar=True)
 
     return dict(
         times=sim.trange(),
         tar=sim.data[pTar],
         ens=sim.data[pEns])
 
-def run(N=3000, neuron_type=LIF(), tTrain=100, tTest=200, tTransTrain=0, tTransTest=20, nTrain=5, nTest=5, f=DoubleExp(1e-3, 1e-1), dt=0.001, dtSampleTrain=0.003, dtSampleTest=0.01, seed=0, f2=10, reg=1e-2, tauRiseMax=1e-2, evals=20, r=30, load=False, file=None):
+def run(N=3000, neuron_type=LIF(), tTrain=100, tTest=100, tTransTrain=20, tTransTest=20,
+    nTrain=1, nTest=10, dt=0.001, dtSampleTrain=0.003, dtSampleTest=0.01, seed=0,
+    f2=10, reg=1e-3, reg2=1e-3, evals=30, r=30, load=False, file=None,
+    tauRiseMin=1e-2, tauRiseMax=3e-2, tauFallMin=1e-1, tauFallMax=3e-1):
 
     print('\nNeuron Type: %s'%neuron_type)
     rng = np.random.RandomState(seed=seed)
+    timeStepsTrain = int((tTrain-tTransTrain)/dtSampleTrain)
+    tStart = int(tTransTrain/dtSampleTrain)
+    tFlat = int((tTrain-tTransTrain)/dtSampleTrain)*nTrain
     if load:
-        d1 = np.load(file)['d1']
+        d = np.load(file)['d']
         d2 = np.load(file)['d2']
-        tauRise1 = np.load(file)['tauRise1']
-        tauFall1 = np.load(file)['tauFall1']
-        f1 = DoubleExp(tauRise1, tauFall1)
+        tauRise = np.load(file)['tauRise']
+        tauFall = np.load(file)['tauFall']
+        f = DoubleExp(tauRise, tauFall)
     else:
         print('decoders for ens')
-        spikes = np.zeros((nTrain, int((tTrain-tTransTrain)/dtSampleTrain), N))
-        spikes2 = np.zeros((nTrain, int((tTrain-tTransTrain)/dtSampleTrain), N))
-        targets = np.zeros((nTrain, int((tTrain-tTransTrain)/dtSampleTrain), 3))
-        targets2 = np.zeros((nTrain, int((tTrain-tTransTrain)/dtSampleTrain), 3))
+        spikes = np.zeros((nTrain, timeStepsTrain, N))
+        spikes2 = np.zeros((nTrain, timeStepsTrain, N))
+        targets = np.zeros((nTrain, timeStepsTrain, 3))
+        targets2 = np.zeros((nTrain, timeStepsTrain, 3))
         for n in range(nTrain):
             IC = np.array([rng.uniform(-15, 15), rng.uniform(-20, 20), rng.uniform(10, 35)])
-            data = go(N=N, neuron_type=neuron_type, l=True, t=tTrain, f=f, r=r, dt=dt, dtSample=dtSampleTrain, seed=seed, IC=IC)
-            spikes[n] = data['ens'][int(tTransTrain/dtSampleTrain):]
-            spikes2[n] = gaussian_filter1d(data['ens'], sigma=f2, axis=0)[int(tTransTrain/dtSampleTrain):]
-            targets[n] = f.filt(data['tar'], dt=dtSampleTrain)[int(tTransTrain/dtSampleTrain):]
-            targets2[n] = gaussian_filter1d(data['tar'], sigma=f2, axis=0)[int(tTransTrain/dtSampleTrain):]
-        d1, f1, tauRise1, tauFall1, X, Y, error = decode(spikes, targets, nTrain, dt=dtSampleTrain, dtSample=dtSampleTrain, name="lorenzNew", evals=evals, tauRiseMax=tauRiseMax)
-        spikes2 = spikes2.reshape((int((tTrain-tTransTrain)/dtSampleTrain)*nTrain), N)
-        targets2 = targets2.reshape((int((tTrain-tTransTrain)/dtSampleTrain)*nTrain, 3))
-        d2, _ = nengo.solvers.LstsqL2(reg=reg)(spikes2, targets2)
-        np.savez("data/lorenzNew_%s.npz"%neuron_type, d1=d1, d2=d2, tauRise1=tauRise1, tauFall1=tauFall1, f2=f2)
-        
-        plot3D(X, Y, neuron_type, suffix="train")
-        plotPairwise(X, Y, neuron_type, suffix="train")
-        error = plotTent(spikes2, targets2, d2, neuron_type, suffix="train")
-    
+            data = go(N=N, neuron_type=neuron_type, l=True, t=tTrain, r=r, dt=dt, dtSample=dtSampleTrain, seed=seed, IC=IC)
+            spikes[n] = data['ens'][-timeStepsTrain:]
+            spikes2[n] = gaussian_filter1d(data['ens'], sigma=f2, axis=0)[-timeStepsTrain:]
+            targets[n] = data['tar'][-timeStepsTrain:]
+            targets2[n] = gaussian_filter1d(data['tar'], sigma=f2, axis=0)[-timeStepsTrain:]
+        d, f, tauRise, tauFall, X, Y, error = decode(
+            spikes, targets, nTrain, dt=dtSampleTrain, dtSample=dtSampleTrain, name="lorenzNew", evals=evals, reg=reg,
+            tauRiseMin=tauRiseMin, tauRiseMax=tauRiseMax, tauFallMin=tauFallMin, tauFallMax=tauFallMax)
+        spikes2 = spikes2.reshape((tFlat, N))
+        targets2 = targets2.reshape((tFlat, 3))
+        A2 = gaussian_filter1d(spikes2, sigma=f2, axis=0)[-timeStepsTrain:]
+        Y2 = gaussian_filter1d(targets2, sigma=f2, axis=0)[-timeStepsTrain:]
+        d2, _ = nengo.solvers.LstsqL2(reg=reg2)(spikes2, targets2)
+        np.savez("data/lorenzNew_%s.npz"%neuron_type, d=d, d2=d2, tauRise=tauRise, tauFall=tauFall, f2=f2)
+        X2 = np.dot(A2, d2)[-timeStepsTrain:]
+        error = plotLorenz(X, X2, Y2, neuron_type, "train")
+
     print("testing")
+    tStart = int(tTransTest/dtSampleTest)
     errors = np.zeros((nTest))
+    rng = np.random.RandomState(seed=100+seed)
     for test in range(nTest):
         IC = np.array([rng.uniform(-15, 15), rng.uniform(-20, 20), rng.uniform(10, 35)])
-        data = go(d1=d1, f1=f1, N=N, neuron_type=neuron_type, t=tTest, f=f, r=r, dt=dt, dtSample=dtSampleTest, seed=seed, IC=IC)
-        A = f1.filt(data['ens'], dt=dtSampleTest)
-        X = np.dot(A, d1)[int(tTransTest/dtSampleTest):]
-        Y = f.filt(data['tar'], dt=dtSampleTest)[int(tTransTest/dtSampleTest):]
-        X2 = gaussian_filter1d(data['ens'], sigma=f2, axis=0)[int(tTransTest/dtSampleTest):]
-        Y2 = gaussian_filter1d(data['tar'], sigma=f2, axis=0)[int(tTransTest/dtSampleTest):]
-        plot3D(X, Y, neuron_type, suffix="test%s"%test)
-        plotPairwise(X, Y, neuron_type, suffix="test%s"%test)
-        error = plotTent(X2, Y2, d2, neuron_type, suffix="test%s"%test)
+        data = go(d=d, f=f, N=N, neuron_type=neuron_type, t=tTest, r=r, dt=dt, dtSample=dtSampleTest, seed=seed, IC=IC)
+        A = f.filt(data['ens'], dt=dtSampleTest)
+        A2 = gaussian_filter1d(data['ens'], sigma=f2, axis=0)[tStart:]
+        X = np.dot(A, d)[tStart:]
+        X2 = np.dot(A2, d2)[tStart:]
+        Y = data['tar'][tStart:]
+        Y2 = gaussian_filter1d(data['tar'], sigma=f2, axis=0)[tStart:]
+        error = plotLorenz(X, X2, Y2, neuron_type, test)
         errors[test] = error
+    _ = plotLorenz(Y, Y2, Y2, 'target', '')
     print('errors: ', errors)
-    np.savez("data/lorenzNew_%s.npz"%neuron_type, d1=d1, d2=d2, tauRise1=tauRise1, tauFall1=tauFall1, f2=f2, errors=errors)
+    np.savez("data/lorenzNew_%s.npz"%neuron_type, d=d, d2=d2, tauRise=tauRise, tauFall=tauFall, f2=f2, errors=errors)
     return errors
 
-errorsLIF = run(neuron_type=LIF(), N=2000, tTrain=300, nTrain=3, nTest=10, load=False, file="data/lorenzNew_LIF().npz")
-# errorsALIF = run(neuron_type=ALIF(), load=True, file="data/lorenzNew_ALIF().npz")
-#errorsWilson = run(neuron_type=Wilson(), dt=1e-4, r=25, load=False, file="data/lorenzNew_Wilson().npz")
+errorsLIF = run(neuron_type=LIF(), load=False, file="data/lorenzNew_LIF().npz")
+errorsALIF = run(neuron_type=ALIF(), load=False, file="data/lorenzNew_ALIF().npz")
+errorsWilson = run(neuron_type=Wilson(), dt=1e-4, load=False, file="data/lorenzNew_Wilson().npz")
